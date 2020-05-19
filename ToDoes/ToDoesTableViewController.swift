00c19acfec27a8f3b7ea7026833a1d912cf30d5a
EllenZhangYarong <ellenzhg@hotmail.com>
2020-05-19 10:23:37 +0800

    Complete with datasource and delegate methods

---
+++ ToDoes/ToDoesTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ToDoesTableViewController.swift
+//  ToDoes
+//
+//  Created by Ellen Zhang on 19/05/20.
+//  Copyright © 2020 Ellen Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ToDoesTableViewController: UITableViewController {
+    
+    var toDoesArray = ["shopping", "laundry", "assist math"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDoesArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+
+        cell.textLabel?.text = toDoesArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        print(toDoesArray[indexPath.row])
+    }
+
+}
